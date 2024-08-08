759afc0b766f190c2923c6a077fcab42412ab7f5
Brian Randell (MCW) <brianr@mcwtech.com>
2024-08-08 08:41:07 -0700

    New clean OOB app

---
+++ src/ghaspub/ghaspubweb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ghaspubweb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ src/ghaspub/ghaspubweb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ghaspubweb.MvcApplication" Language="C#" %>
